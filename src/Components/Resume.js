5ef69289dda3c900bda1d44af4c3226d42c3eebd
yoni6232 <yoni6232@gmail.com>
2020-08-18 09:53:29 +0300

    change

---
+++ src/Components/Resume.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+ 
+  render() {
+
+    if(this.props.data){
+      var skillmessage = this.props.data.skillmessage;
+      var education = this.props.data.education.map(function(education){
+        return <div key={education.school}><h3>{education.school}</h3>
+        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+        <p>{education.description}{education.description1}</p></div>
+      })
+      var work = this.props.data.work.map(function(work){
+        return <div key={work.company}>
+          <h3>{work.company}</h3>
+            <p className="info">{work.title}<span>&bull;
+              </span> <em className="date">{work.years}</em>
+              </p>
+              {work.description}<br/>  
+             {work.description1}<br/>
+             {work.description2}<br/>
+             {work.description3}<br/>
+             {work.description4}
+
+
+
+        </div>
+      })
+ 
+    }
+
+    return (
+      <section id="resume">
+
+      <div className="row education">
+         <div className="three columns header-col">
+            <h1><span>Education</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+            <div className="row item">
+               <div className="twelve columns">
+                 {education}
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      <div className="row work">
+
+         <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+          {work}
+        </div>
+        
+    </div>
+   </section>
+    );
+  }
+}
+
+export default Resume;
